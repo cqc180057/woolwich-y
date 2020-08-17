0942ef8eeae5aa8c094681e98758f82d8cedf8fa
cqc180057 <65412893+cqc180057@users.noreply.github.com>
2020-08-18 00:53:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib
 
-
-def main():
-    st.title("Binary Classification Web App")
-    st.sidebar.title("Binary Classification Web App")
-    st.markdown("Are your mushrooms edible or poisonous? 🍄")
-    st.sidebar.markdown("Are your mushrooms edible or poisonous? 🍄")
-
-
-
-if __name__ == '__main__':
-    main()
-    
+st.title("Binary Classification Web App")
+st.sidebar.title("Binary Classification Web App")
+ 
+ 
     
